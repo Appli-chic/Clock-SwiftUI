728bcdf052cb7436516c1283aa606e8e3c181e52
Applichic <applichic@gmail.com>
2020-06-03 08:33:31 +0200

    Initial Commit

---
+++ Clock/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Clock
+//
+//  Created by Lazyos on 6/3/20.
+//  Copyright © 2020 Applichic. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
